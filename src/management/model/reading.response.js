901dadd1e1b4780cd1fdbe29b1690cf9c69ff94c
Christopher <79271081+ChrisByBits@users.noreply.github.com>
2025-11-10 20:22:33 -0500

    refactor: fix reading event

--- src/management/model/reading.response.js
+++ src/management/model/reading.response.js
@@ -3,6 +3,8 @@ export class ReadingResponse {
     this.localId = localId;
     this.sensorTypeId = sensorTypeId;
     this.message = message;
-    this.timestamp =`${timestamp.split('T')[0] } ${timestamp.split('T')[1].split(':')[0]}:${timestamp.split('T')[1].split(':')[1]}`
+    this.timestamp = timestamp
+      ? `${timestamp.split('T')[0]} ${timestamp.split('T')[1].split(':')[0]}:${timestamp.split('T')[1].split(':')[1]}`
+      : new Date().toLocaleString();
   }
 }
